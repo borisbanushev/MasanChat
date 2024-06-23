2df3e1a56f994a9c35f854d28d2d1c9a372f60de
borisbanushev <borisbanushev>
2024-06-23 07:41:18 +0000

    2nd

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,9 @@ import streamlit as st
 
 st.title("Masan Chatbot")
 
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
